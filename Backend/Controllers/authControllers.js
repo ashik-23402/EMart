cc1beb76956d2f2b03552224c432bf0308bcfc07
Ashik <ashik23402@gmail.com>
2023-11-18 18:42:03 +0600

    login done

--- Backend/Controllers/authControllers.js
+++ Backend/Controllers/authControllers.js
@@ -2,6 +2,8 @@
 const User = require('../Models/User');
 const bcrypt = require('bcryptjs');
 const jsonwebToken = require('jsonwebtoken');
+const CustomError = require('../Utils/CustomError')
+
 
 exports.hello=(req,res,next)=>{
 
@@ -61,5 +63,62 @@ exports.registerUser=async(req,res,next)=>{
         });
         
     }
+
+}
+
+
+exports.login = async(req,res,next)=>{
+
+    try {
+
+    const {email,password} = req.body;
+
+    if(!email || !password){
+
+        return next(new CustomError("field cannot be empty",500));
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        return next(new CustomError("invalid email and password",401));
+    }
+
+    const ispasswordMatch = await bcrypt.compare(password,user.password);
+
+    if(!ispasswordMatch){
+        return next(new CustomError("invalid email and password",401));
+    }
+
+    const token = jsonwebToken.sign({id:user._id},process.env.secretkey,
+        { expiresIn:process.env.jwtexpire});
+
+    const cookieOptions = {
+            maxAge:24*60*60*1000,
+            httpOnly:true
+        };
+
+
+    res.status(201).cookie("token",token,cookieOptions).json({
+            success:true,
+            accessToken:token,
+           
+        });
+    
+
+
+
+    }catch (error) {
+
+        console.log("I am from login controller error ");
+
+        res.status(500).json({
+            error:"internal server error"
+        });
+
+
+    }
+  
+
 
 }
--- Backend/Routers/authRoute.js
+++ Backend/Routers/authRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { hello, registerUser } = require('../Controllers/authControllers');
+const { hello, registerUser, login } = require('../Controllers/authControllers');
 
 const router = express.Router();
 
 
 router.route("/test").get(hello);
 router.route("/register").post(registerUser);
+router.route("/login").post(login);
 
 
 module.exports = router;
